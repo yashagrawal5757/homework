f1460f48f78b853af6ff946af0aa550561392641
Yash Agrawal <yashagrawal5757@gmail.com>
2023-02-07 14:06:39 -0500

    Update model_factory.py

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,4 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 64, output_dim, 2, torch.nn.ReLU, torch.nn.init.uniform_)
+    return MLP(input_dim, 64, output_dim, 1, torch.nn.ReLU, torch.nn.init.ones_)
