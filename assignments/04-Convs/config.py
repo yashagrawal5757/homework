342e2637823b2280427e20277b89e530b0da50bc
Yash Agrawal <yashagrawal5757@gmail.com>
2023-03-17 19:55:58 -0400

    Add files via upload

---
+++ assignments/04-Convs/config.py
@@ -0,0 +1,21 @@
+from typing import Callable
+import torch
+import torch.optim
+import torch.nn as nn
+from torchvision.transforms import Compose, ToTensor
+
+
+class CONFIG:
+    """
+    Config class
+    """
+
+    batch_size = 8
+    num_epochs = 8
+    num_channels = 3
+
+    optimizer_factory: Callable[
+        [nn.Module], torch.optim.Optimizer
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    transforms = Compose([ToTensor()])
